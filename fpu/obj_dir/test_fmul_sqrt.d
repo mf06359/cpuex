de402d52be0dde1f6e0c6be31031e6826f79aa9e
mf06359 <mightyworksmf@gmail.com>
2026-03-10 14:27:36 +0900

    make clean!

--- fpu/obj_dir/test_fmul_sqrt.d
+++
@@ -1,9 +0,0 @@
-test_fmul_sqrt.o: ../test_fmul_sqrt.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vtop_fmul_sqrt.h
