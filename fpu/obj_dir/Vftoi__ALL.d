d619db0e52b260127a2b3be40d96aa5f6bee6b8a
mf06359 <mightyworksmf@gmail.com>
2026-03-12 01:32:19 +0900

    ftoi modified

--- fpu/obj_dir/Vftoi__ALL.d
+++
@@ -1,9 +0,0 @@
-Vftoi__ALL.o: Vftoi__ALL.cpp Vftoi.cpp Vftoi__pch.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.046/share/verilator/include/verilated_funcs.h \
-  Vftoi__Syms.h Vftoi.h Vftoi___024root.h Vftoi___024root__0.cpp \
-  Vftoi___024root__Slow.cpp Vftoi___024root__0__Slow.cpp \
-  Vftoi__Syms__Slow.cpp
